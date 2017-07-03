40df42ea06776142a8e70c91d1cb5755ee3c3968
glegendre <kubitusleroi@laposte.net>
2017-07-03 18:02:19 +0200

    message

---
+++ exercice4/exo4.c
@@ -0,0 +1,16 @@
+                                                                                                                                                                          void myputstr(const char str[])                                                                                              
+ {  
+                                                                                                                                                                         
+ int i = 0;                                                                                                                                                                                                                                                                                                                                   
+ while (str[i] != '\0')                                                                                                                                                    { 
+	write(1,&str[i],1);
+	i = i+1;
+ }
+ }     
+
+int main (void)
+
+{
+     myputstr("salut\n");  
+	return (0);
+}
